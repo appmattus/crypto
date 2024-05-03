49f5c5daf5c465c0fe19778e6f884a19889184c5
Matthew Dolan <info@mattdolan.com>
2024-05-03 07:52:17 +0100

    Fix build issue

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Appmattus Limited
+ * Copyright 2021-2024 Appmattus Limited
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,10 +22,6 @@ plugins {
 
 repositories {
     mavenCentral()
-}
-
-kotlinDslPluginOptions {
-    experimentalWarning.set(false)
 }
 
 tasks.withType(KotlinCompile::class.java).all {
