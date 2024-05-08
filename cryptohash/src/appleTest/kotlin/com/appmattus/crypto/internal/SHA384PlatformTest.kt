f41dfa1eb36ea8c912d542480b8ca6a83d98d313
Matthew Dolan <info@mattdolan.com>
2024-05-08 17:20:20 +0100

    Restructure main project to use built-in hierarchy Re-enable some iOS tests

--- cryptohash/src/appleTest/kotlin/com/appmattus/crypto/internal/SHA384PlatformTest.kt
+++ cryptohash/src/appleTest/kotlin/com/appmattus/crypto/internal/SHA384PlatformTest.kt
@@ -1,10 +1,7 @@
 package com.appmattus.crypto.internal
 
 import com.appmattus.crypto.internal.core.ios.SHA384
-import kotlin.test.Ignore
 
-// Crashes on iOS
-@Ignore
 internal class SHA384PlatformTest : SHA384Test() {
     override fun digest() = SHA384()
 }
